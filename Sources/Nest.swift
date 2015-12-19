a81f852418be126cf67747510a2d9481621f28cb
Kyle Fuller <kyle@fuller.li>
2015-12-19 15:59:38 +0000

    Implement NEP2

--- Sources/Nest.swift
+++ Sources/Nest.swift
@@ -1,12 +1,20 @@
 /// Represents a HTTP Header, Key and Value
 public typealias Header = (String, String)
 
+
+/// Represents a HTTP request or response body
+public protocol PayloadType {
+  /// Returns the next byte in the payload
+  mutating func next() -> Int8?
+}
+
+
 /// Represents a HTTP Request
 public protocol RequestType {
   var method:String { get }
   var path:String { get }
   var headers:[Header] { get }
-  var body:String? { get }
+  var body: PayloadType? { get }
 }
 
 
@@ -14,7 +22,7 @@ public protocol RequestType {
 public protocol ResponseType {
   var statusLine:String { get }
   var headers:[Header] { get }
-  var body:String? { get }
+  var body: PayloadType? { get }
 }
 
 public typealias Application = RequestType -> ResponseType
